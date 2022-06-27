78df01afc97062bf614f378b21d3e874f52c08fb
HenniePenny <h.kressner@hotmail.de>
2022-06-27 21:59:46 +0200

    got stuck, too complicated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //creates a variable for the id for generate pw button -- 
 var generateBtn = document.querySelector("#generate");
 
-//store options for passwords in variables
+//store the different options for character choices as strings in a constant
 let lengthOfPw = document.getElementsByClassName("length");
 
 const lowerCase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
@@ -18,7 +18,10 @@ const specialChar = "!,§,$,%,&,/,?,*,#,-,<,+,;,(,)"
 const specialCharArray = specialChar.split(",");
 
 //make functions for each character type to randomize selection for password
-
+const getRandomLC = function lowerCaseFunc() {
+  return Math.floor(Math.random() * lowerCaseArray.length);
+}
+console.log (getRandomLC);
 
 //store all separate pw character options in one single var for easier randomization
 
@@ -38,12 +41,43 @@ document.addEventListener("click", pwFunction);
 //1st prompt: length of pw 8-128 characters
 pwLength = document.getElementById("pwlength")
 
-
+//make sure that at least one box is checked
 //2nd prompt: lower case letters? + validation
 //3rd prompt: upper case letters? + validation
 //4th prompt: numbers? + validation
 //5th prompt: and/or special characters? + validation
 //choose at least one
+/*const lower = document.getElementById("lowerCheck").checked;
+const upper = document.getElementById("upperCheck").checked;
+const number = document.getElementById("numbersCheck").checked;
+const symbol = document.getElementById("specialCheck").checked;
+if (lower + upper + number + symbol === 0) {
+  alert("You must check at least one box!");
+}
+  else   
+    function doSomeThing()
+    {
+        //do something -- include this in a div or something in the html
+        document.getElementById("displayText1").innerHTML = text1;
+        //do something
+        document.getElementById("displayText2").innerHTML = text2;
+        //do something
+        document.getElementById("displayText3").innerHTML = text3;
+    }
+    
+    //HTML ?
+    <span id="displayText1"></span>
+    <span id="displayText2"></span>
+    <span id="displayText3"></span>
+
+
+
+
+  }
+  return;
+}
+
+const lower =*/
 
 //after all prompts generate password
 
