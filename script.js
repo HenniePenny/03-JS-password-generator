993909d59ee7383adb4196d678cf916841c7a5c3
HenniePenny <h.kressner@hotmail.de>
2022-07-01 11:26:22 +0200

    include alerts, still need work

--- script.js
+++ script.js
@@ -13,23 +13,72 @@ const specialCharOptions = ["!", "§", "$", "%", "&", "/", "?", "*", "#", "-", "
 /*make prompt for pw length and store the value in a let, make it a function to only allow numbers between 8 and 128; 
 if not entered, alert what the input needs to be;*/
 // TODO: make it a loop so that it starts a the beginning if the criteria are not met
-let lengthInput = prompt("Please choose your password length by entering a number between 8 and 128.");
 
-if (lengthInput < 8) {
+const getLength = function() {
+  let lengthInput = prompt("Please choose your password length by entering a number between 8 and 128.");
+  if (lengthInput < 8) {
     alert ("Your password must be at least 8 characters long.");
+    getLength();
   } else if (lengthInput > 129) {
       alert ("Your password can contain a maximum of 128 characters.");
+      getLength();
   } else{
     alert ("Your password will contain " + lengthInput + " characters.");
-};
-  
+  };
+  return lengthInput;
+}
+
+
+const allowedCharacters = function() {
+  let lowerCaseLetters = confirm("Do you want to include lower case letters in your password? (e.g.: abcdef)");
+  if (confirm(lowerCaseLetters) == true) {
+    alert ("Your password will include lower case letters.");
+    allowedCharacters();
+  } else {
+    alert ("Your password will not include lower case letters.");
+  };
+      
+  let upperCaseLetters = confirm("Do you want to include upper case letters in your password? (e.g.: ABCDEF)");
+  if (confirm(upperCaseLetters) == true) {
+    alert ("Your password will upper lower case letters.");
+  } else {
+    alert ("Your password will not include upper case letters.");
+  }
+
+  let numericCharacters = confirm("Do you want to include numbers in your password? (e.g. 123456");
+  if (confirm(numericCharacters) == true) {
+    alert ("Your password will include numbers.");
+  } else {
+    alert ("Your password will not include numbers.");
+  }
+
+  let specialCharacters = confirm("Do you want to include special characters in your password? (e.g. !?&%*#/");
+  if (confirm(specialCharacters) == true) {
+    alert ("Your password will include special characters.");
+  } else {
+    alert ("Your password will not include special characters.");
+  }
+  return(allowedCharacters)
+}
+
+
+const generatePassword = function() {
+  let passwordLength = getLength();
+  let passwordCharacters = allowedCharacters();
+  console.log(passwordCharacters);
+}
 //make prompts for the character options
+
+
 
 //2nd prompt: lower case letters? + validation
 //3rd prompt: upper case letters? + validation
 //4th prompt: numbers? + validation
 //5th prompt: and/or special characters? + validation
 //choose at least one
+
+
+
 
 /*const lower = document.getElementById("lowerCheck").checked;
 const upper = document.getElementById("upperCheck").checked;
